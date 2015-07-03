7970cc636a5ce712c061b9d174dc17117b122b8c
Chris Malley <cmalley@pixelzoom.com>
2015-07-03 09:40:30 -0600

    move phetbrand/brand 'master' to phetsims/brand 'phet', phetsims/tasks#256

--- js/Brand.js
+++ js/Brand.js
@@ -1,18 +1,22 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
 // Returns branding information for the simulations, see https://github.com/phetsims/brand/issues/1
-define( function() {
+define( function( require ) {
   'use strict';
 
-  //TODO: Add your group's information here and it will appear in the About Dialog
+  var phetString = 'PhET<sup>\u00ae</sup> Interactive Simulations'; // as in Java sims, do not internationalize
+  var copyrightString = 'Copyright © 2004-2015 University of Colorado Boulder'; // as in Java sims, do not internationalize
+
+  var termsPrivacyAndLicensingString = require( 'string!JOIST/termsPrivacyAndLicensing' );
+
+  // constants
+  var SOFTWARE_LICENSING_URL = 'http://phet.colorado.edu/en/html-terms-privacy-and-licensing';
+
   return {
-    name: '',//For example: My Company
-    copyright: '',//For example: Copyright © 2014, My Company
+    name: phetString,
+    copyright: copyrightString,
     links: [
-      {
-        text: '', //For example "My Company Support"
-        url: ''
-      } //For example, https://www.mycompany.com/support
+      { text: termsPrivacyAndLicensingString, url: SOFTWARE_LICENSING_URL }
     ],
     adaptedFromPhET: false
   };
