30c7d1f9444882acfd0188e3a0e0fc52102c96e2
Sam Reid <reids@colorado.edu>
2015-07-08 10:16:35 -0600

    Added id field for phet brand, see https://github.com/phetsims/joist/issues/271

--- js/Brand.js
+++ js/Brand.js
@@ -6,6 +6,7 @@ define( function() {
 
   //TODO: Add your brand's information here and it will appear in the About Dialog
   return {
+    id: 'phet',
     name: null, // {string} For example: 'My Company'
     copyright: null, // {string} For example: 'Copyright © 2014, My Company'
     links: null, // {Object[]} For example: { text: 'My Company Support', url: 'https://www.mycompany.com/support' }
