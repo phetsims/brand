04137a362557dd00f2b93e5aca25ebd51061f36e
zepumph <michael.kauzmann@colorado.edu>
2017-06-15 15:27:57 -0800

    noBrand hack, https://github.com/phetsims/scenery/issues/628

---
+++ noBrand/js/Brand.js
@@ -0,0 +1,20 @@
+// Copyright 2002-2017, University of Colorado Boulder
+
+// Returns branding information for the simulations, see https://github.com/phetsims/brand/issues/1
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var brand = require( 'BRAND/../../js/brand' );
+
+  // Documentation for all properties is available in brand/adapted-from-phet/js/Brand.js
+  var Brand = {
+    id: 'noBrand',
+    name: 'No Brand', // no i18n
+    phetioEnabled: false
+  };
+
+  brand.register( 'Brand', Brand );
+
+  return Brand;
+} );
