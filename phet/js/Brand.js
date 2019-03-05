1758ed460af9e3caf942a6313353a19da487bb43
denz1994 <denz1994@gmail.com>
2019-03-05 13:44:32 -0500

    Updating TM logo and kebab menu position. See https://github.com/phetsims/brand/issues/34

--- phet/js/Brand.js
+++ phet/js/Brand.js
@@ -15,7 +15,7 @@ define( function( require ) {
   // Documentation for all properties is available in brand/adapted-from-phet/js/Brand.js
   var Brand = {
     id: 'phet',
-    name: 'PhET<sup>\u00ae</sup> Interactive Simulations', // no i18n
+    name: 'PhET\u2122 Interactive Simulations', // no i18n
     copyright: 'Copyright © 2004-2016 University of Colorado Boulder', // no i18n
     isPhetApp: phet.chipper.queryParameters[ 'phet-app' ] || phet.chipper.queryParameters[ 'phet-android-app' ],
     getLinks: function( simName, locale ) {
