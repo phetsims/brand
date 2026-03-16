dd76494a9e93700b2f3c745dd6ca203744ffb387
Jonathan Olson <jonathan.olson@colorado.edu>
2026-03-16 17:38:23 -0600

    AboutDialog/Brand license section and improvements for CC BY-NC changes, see https://github.com/phetsims/special-ops/issues/318

--- phet-io/js/Brand.js
+++ phet-io/js/Brand.js
@@ -9,33 +9,42 @@ define( function( require ) {
 
   // strings
   var termsPrivacyAndLicensingString = require( 'string!JOIST/termsPrivacyAndLicensing' );
+  var privacyPolicyLinkString = require( 'string!JOIST/privacyPolicy' );
   var translationCreditsLinkString = require( 'string!JOIST/translation.credits.link' );
   var thirdPartyCreditsLinkString = require( 'string!JOIST/thirdParty.credits.link' );
+  var donateToPhetLinkString = require( 'string!JOIST/donateToPhet' );
 
   // Documentation for all properties is available in brand/adapted-from-phet/js/Brand.js
   var Brand = {
     id: 'phet-io',
-    name: 'PhET\u2122 Interactive Simulations', // no i18n
-    copyright: 'Copyright © 2004-2016 University of Colorado Boulder',
-    additionalLicenseStatement: 'This PhET-iO interoperable simulation file requires a license.\n' +
-                                'USE WITHOUT A LICENSE AGREEMENT IS STRICTLY PROHIBITED.\n' +
-                                'Contact phethelp@colorado.edu regarding licensing.',
+    name: 'PhET\u2122 Interactive Simulations, University of Colorado Boulder', // no i18n
+    copyright: 'Copyright © 2002-{{year}} Regents of the University of Colorado',
+    license: 'Use of this PhET-iO interoperable simulation file requires a license from the University of Colorado Boulder.<br>' +
+             '<b>USE WITHOUT A LICENSE AGREEMENT IS STRICTLY PROHIBITED.</b><br>' +
+             'For information on PhET-iO simulation and/or brand licensing, see <a href="https://phet.colorado.edu/en/licensing"><u>Licensing</u></a>.',
+    licenseWithoutLinks: 'Use of this PhET-iO interoperable simulation file requires a license from the University of Colorado Boulder.<br>' +
+                         '<b>USE WITHOUT A LICENSE AGREEMENT IS STRICTLY PROHIBITED.</b><br>' +
+                         'For information on PhET-iO simulation and/or brand licensing, see Licensing. https://phet.colorado.edu/en/licensing',
     getLinks: function( simName, locale ) {
       return [
         {
-          text: termsPrivacyAndLicensingString,
-          url: 'http://phet.colorado.edu/en/licensing/html'
+          text: privacyPolicyLinkString,
+          url: 'https://phet.colorado.edu/en/privacy-policy'
         },
         {
           text: translationCreditsLinkString,
-          url: 'http://phet.colorado.edu/translation-credits?simName=' + encodeURIComponent( simName ) + '&locale=' + encodeURIComponent( locale )
+          url: 'http://phet.colorado.edu/translation-credits?simName=' + encodeURIComponent( simName ) +
+               '&locale=' + encodeURIComponent( locale )
         },
         {
           text: thirdPartyCreditsLinkString,
           url: 'http://phet.colorado.edu/third-party-credits?simName=' + encodeURIComponent( simName ) +
                '&locale=' + encodeURIComponent( locale ) + '#' + simName
-        }
-      ];
+        },
+        {
+          text: donateToPhetLinkString,
+          url: 'https://giveto.colorado.edu/phet-interactive-simulations/?a=12323626'
+        } ];
     }
   };
 
