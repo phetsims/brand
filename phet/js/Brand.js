0b1b056374d110be548f5afd479b1bac8937c941
Jonathan Olson <jonathan.olson@colorado.edu>
2026-03-16 17:38:23 -0600

    AboutDialog/Brand license section and improvements for CC BY-NC changes, see https://github.com/phetsims/special-ops/issues/318

--- phet/js/Brand.js
+++ phet/js/Brand.js
@@ -9,21 +9,29 @@ define( function( require ) {
 
   // strings
   var termsPrivacyAndLicensingString = require( 'string!JOIST/termsPrivacyAndLicensing' );
+  var privacyPolicyLinkString = require( 'string!JOIST/privacyPolicy' );
   var translationCreditsLinkString = require( 'string!JOIST/translation.credits.link' );
   var thirdPartyCreditsLinkString = require( 'string!JOIST/thirdParty.credits.link' );
+  var donateToPhetLinkString = require( 'string!JOIST/donateToPhet' );
 
   // Documentation for all properties is available in brand/adapted-from-phet/js/Brand.js
   var Brand = {
     id: 'phet',
-    name: 'PhET\u2122 Interactive Simulations', // no i18n
-    copyright: 'Copyright © 2002-{{year}} University of Colorado Boulder', // no i18n
+    name: 'PhET\u2122 Interactive Simulations, University of Colorado Boulder', // no i18n
+    copyright: 'Copyright © 2002-{{year}} Regents of the University of Colorado', // no i18n
+    license: 'This simulation file is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License (<a href="https://creativecommons.org/licenses/by-nc/4.0/"><u>CC BY-NC 4.0</u></a>).<br>' +
+             '<b>COMMERCIAL USE WITHOUT A COMMERCIAL LICENSE AGREEMENT IS STRICTLY PROHIBITED.</b><br>' +
+             'For information on commercial and/or brand licensing, see <a href="https://phet.colorado.edu/en/licensing"><u>Licensing</u></a>.',
+    licenseWithoutLinks: 'This simulation file is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License (CC BY-NC 4.0). https://creativecommons.org/licenses/by-nc/4.0/<br>' +
+                         '<b>COMMERCIAL USE WITHOUT A COMMERCIAL LICENSE AGREEMENT IS STRICTLY PROHIBITED.</b><br>' +
+                         'For information on commercial and/or brand licensing, see Licensing. https://phet.colorado.edu/en/licensing',
     phetioEnabled: false,
     isPhetApp: phet.chipper.queryParameters[ 'phet-app' ] || phet.chipper.queryParameters[ 'phet-android-app' ],
     getLinks: function( simName, locale ) {
       return [
         {
-          text: termsPrivacyAndLicensingString,
-          url: 'http://phet.colorado.edu/en/licensing/html'
+          text: privacyPolicyLinkString,
+          url: 'https://phet.colorado.edu/en/privacy-policy'
         },
         {
           text: translationCreditsLinkString,
@@ -34,6 +42,10 @@ define( function( require ) {
           text: thirdPartyCreditsLinkString,
           url: 'http://phet.colorado.edu/third-party-credits?simName=' + encodeURIComponent( simName ) +
                '&locale=' + encodeURIComponent( locale ) + '#' + simName
+        },
+        {
+          text: donateToPhetLinkString,
+          url: 'https://giveto.colorado.edu/phet-interactive-simulations/?a=12323626'
         } ];
     }
   };
